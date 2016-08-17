a929abd3d97c1fb4df3d19a7a91df2dfa8e5f5dd
cpppy <1107706059@qq.com>
2016-08-17 08:38:29 +0800

    Create 275_H-Index_II.cc

---
+++ 275_H-Index_II.cc
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int h=0;
+        for(int i=citations.size()-1;i>=0;--i){
+            if(citations[i]>h) h++;
+        }
+        return h;
+    }
+};
