89cf9f95a430f6f6fc2fb28442e0b660a766b599
cpppy <1107706059@qq.com>
2016-08-18 19:11:06 +0800

    Create 48_Rotate_Image.cc
    https://leetcode.com/problems/rotate-image/

---
+++ 48_Rotate_Image.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        vector<vector<int>> copymat=matrix;
+        int n=matrix.size();
+        for(int i=0;i<n;++i){
+            for(int j=0;j<n;++j){
+                matrix[j][n-1-i]=copymat[i][j];
+            }
+        }
+        
+    }
+};
