9f2372222a9e547d848c78be6d3af54971c6430e
cpppy <1107706059@qq.com>
2016-08-22 23:31:07 +0800

    Create 136_Single_Number.cc

---
+++ 136_Single_Number.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.empty())    return NULL;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;++++i){
+            if(nums[i]!=nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[nums.size()-1];
+    }
+};
